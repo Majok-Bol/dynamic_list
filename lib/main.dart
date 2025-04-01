c1865b09d4a1cb346c4a0d915186cdac6cac911b
Majok-Bol <bolmajok001@gmail.com>
2025-04-01 20:14:07 +0300

    added items to the list

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,50 @@ void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+  //sample data:list containing headings and items
+  final List<ListItem>items=[
+    HeadingItem('Fruits'),
+    SubItem("Apple"),
+    SubItem("Cherry"),
+    SubItem("Pears"),
+    SubItem("Pineapple"),
+    SubItem("Watermelon"),
+    HeadingItem('Vegetables'),
+    SubItem('Cabbage'),
+    SubItem('Spinach'),
+    SubItem('Broccoli'),
+    SubItem('Pumpkin'),
+    HeadingItem('Dairy'),
+    SubItem('Milk'),
+    SubItem('Cheese'),
+
+
+  ];
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('Dynamic list items',style: TextStyle(color: Colors.white,fontSize: 25),),backgroundColor: Colors.purple,centerTitle: true,),
+
       ),
     );
   }
+}
+
+//define abstract class for different  list items
+abstract class ListItem{
+
+}
+//heading item
+class HeadingItem extends ListItem{
+  final String title;
+  //constructor for heading title
+  HeadingItem(this.title);
+}
+//sub item under a heading
+class SubItem extends ListItem{
+  final String subItemContent;
+  //constructor for sub item content
+SubItem(this.subItemContent);
 }
